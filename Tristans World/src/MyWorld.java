95a9d386aa664bf01c8864ff3de9f766aa2edd59
Tristan Collery <tristancollery@gmail.com>
2012-06-27 08:53:41 +0100

    Made some changes to the code on the iMac

--- Tristans World/src/MyWorld.java
+++ Tristans World/src/MyWorld.java
@@ -8,7 +8,7 @@ public class MyWorld {
 		System.out.println("This is my world");
 		System.out.println("Back on Track");
 		System.out.println("Samll test change");
-
+		System.out.println("Made a code change on the iMac");
 	}
 
 }
