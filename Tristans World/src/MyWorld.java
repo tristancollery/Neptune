e4183adeed00581e723e769867f975c17585201b
Tristan Collery <tristancollery@gmail.com>
2012-06-27 08:45:10 +0100

    Now committing correctly
    Now committing correctly from Mac

---
+++ Tristans World/src/MyWorld.java
@@ -0,0 +1,14 @@
+
+public class MyWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my world");
+		System.out.println("Back on Track");
+		System.out.println("Samll test change");
+
+	}
+
+}
